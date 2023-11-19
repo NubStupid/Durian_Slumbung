2bc218fc66f6825298b5e02a9f2a0af4e053a50f
NubStupid <ferdinandsh04@gmail.com>
2023-11-19 20:43:03 +0700

    Added content to homepage and footer to template

--- laravel/resources/views/homepage.blade.php
+++ laravel/resources/views/homepage.blade.php
@@ -5,4 +5,83 @@
         $title = "Home";
     @endphp
 @endsection
+@push('style')
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+@endpush
+@section('content')
+    <script>
+        AOS.init();
+    </script>
+    <div class="d-flex justify-content-center my-5">
+        <div class="container-md p-0 m-0">
+            <video preload="TRUE" muted loop autoplay playsinline style="width: 100%;">
+                <source src="{{asset('assets/homepage/Home_Video.mp4')}}" type="video/mp4" class="video">
+            </video>
+            <div class="position-absolute start-50 top-50 translate-middle text-white">
+                <h1 style="font-size: 58px;" data-aos="fade-down" data-aos-duration="2000" data-aos-once="true" data-aos-offset="-150"><b>DURIAN SLUMBUNG</b></h1>
+                <h3 class="text-center" data-aos="fade-down" data-aos-duration="2000" data-aos-once="true" data-aos-offset="-150" style=""><b>Durian Kebanggaan Kami</b></h3>
+            </div>
+        </div>
+    </div>
+    <div class="container-md " style="overflow: hidden;">
+        <div data-aos="fade-right" data-aos-duration="1000" data-aos-once="true" class="pb-5">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-10  bg-green-light rounded-4 rounded-end p-5">
+                        <h2><b>DURIAN YANG MATANG DARI POHONNYA</b></h2>
+                        <p style="font-size: 20px;">
+                        Dengan pengalaman pengelolaan durian lebih dari 30 tahun kami memiliki pengetahuan yang sangat luas tentang durian dan bagaimana cara
+                        mengelolanya dengan baik. Salah satunya yaitu cara agar kami dapat memberikan durian yang memiliki rasa autentik dengan desa kami.
+                        Buah durian yang matang dari pohon dan yang mentah rasanya pasti berbeda. Durian yang diambil matang dari pohon rasanya manis dan legit,
+                        Sedangkan jika diambil saat mentah rasanya manis biasa. Darisitu durian slumbung yang kita miliki memiliki rasa yang khas dan unik bagi
+                        para penggemar buah durian.
+                        </p>
+                    </div>
+                    <div class="col-2 d-flex justify-content-end">
+                        <img src="{{asset('assets/homepage/durian.jpg')}}" alt="durian" srcset="" style="height:250px; max-width:auto;" class="rounded-4 rounded-start z-1">
+                        <div class="bg-green-secondary position-absolute rounded-4" style="right:0;top:5%;width:250px;height:250px;z-index:0;"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div data-aos="fade-left" data-aos-duration="2000" data-aos-once="true" class="pb-5">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-2">
+                        <div class="bg-green-secondary position-absolute rounded-4" style="left:0; top:5%; width:250px; height:250px; z-index:-1;"></div>
+                        <img src="{{asset('assets/homepage/opened-durian.jpg')}}" alt="durian" srcset="" style="height:250px; max-width:auto;" class="rounded-4 rounded-end z-1">
+                    </div>
+                    <div class="col-10 bg-green-light rounded-4 rounded-start p-5" style="z-index:-2;">
+                        <div class="ps-5">
+                            <h2><b>HARGA YANG SANGAT TERJANGKAU</b></h2>
+                            <p style="font-size: 20px;">
+                            Desa Mlancu memiliki lahan pertanian dan perkebunan yang sangat luas dan disana dihasilkan durian yang sangat melimpah,
+                            produk yang kami jual berasal dari petani itu sendiri sehingga anda bisa mendapatkan produk yang fresh dan harga yang sangat terjangkau.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div data-aos="fade-up" data-aos-duration="3000" data-aos-once="true" class="pb-5">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-10  bg-green-light rounded-4 rounded-end p-5">
+                        <h2><b>PROSES KHUSUS PEMANENAN DURIAN SLUMBUNG</b></h2>
+                        <p style="font-size: 20px;">
+                            Kami memperhatikan teknik pemanenan demi menjaga kualitas terbaik dan cita rasa durian slumbung. Diawali dengan proses penalian dimana jika ada salah satu buah durian yang jatuh dari suatu pohon maka dimulai proses penalian setiap buah durian yang ada di pohon tersebut menggunakan tali rafia. Durian yang matang akan lepas dari tangkai pohonnya dan bergelantungan lewat tali rafia tersebut. Darisitu durian dapat dipanen dan cita rasanya tetap terjaga matang sempurna.
+                        </p>
+                    </div>
+                    <div class="col-2 d-flex justify-content-end">
+                        <img src="{{asset('assets/homepage/panen-durian.jpg')}}" alt="durian" srcset="" style="height:250px; max-width:auto;" class="rounded-4 rounded-start z-1">
+                        <div class="bg-green-secondary position-absolute rounded-4" style="right:0;top:5%;width:250px;height:250px;z-index:0;"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
 
--- laravel/resources/views/template/customerTemplate.blade.php
+++ laravel/resources/views/template/customerTemplate.blade.php
@@ -7,7 +7,7 @@
 
     <nav class="navbar navbar-expand-lg bg-green-primary nav-underline">
         <div class="container-fluid">
-        <img class=" ms-3 navbar-brand" src="{{asset('assets/navbar/LogoDurianSlumbung.png')}}" style="max-width:8vw; max-height:auto;">
+        <img class="navbar-brand" src="{{asset('assets/navbar/LogoDurianSlumbung.png')}}" style="max-width:8vw; max-height:auto;">
         @php
             $activeHome = "";
             $activeProduct = "";
@@ -63,7 +63,113 @@
 
 {{-- Footer --}}
 
+<div class="container-fluid py-3 bg-green-primary">
+    <div class="row">
 
+
+        {{-- Bagian Kiri Footer --}}
+
+
+        <div class="col-4">
+            <p class="ms-5 text-light fs-2 fw-bold">DURIAN SLUMBUNG</p>
+            <p class="ms-5 text-light fs-4 fw-semibold">Follow Us On</p>
+            <div class="d-flex flex-row ms-5 mb-3">
+                <a href="https://www.instagram.com/durkedir" class=" pe-2">
+                    <img src="{{asset('assets/footer/instagram.png')}}" alt="ig" srcset="" width="30px" height="30px">
+                </a>
+                <a href="https://www.tiktok.com/@duriankediri" class=" pe-2">
+                    <img src="{{asset('assets/footer/tiktok.png')}}" alt="tt" srcset="" width="30px" height="30px">
+                </a>
+                <a href="https://www.facebook.com/duriankediribergaransi" class=" pe-2">
+                    <img src="{{asset('assets/footer/facebook.png')}}" alt="fb" srcset="" width="30px" height="30px">
+                </a>
+                <a href="https://www.youtube.com/c/NDESOEXPLORE" class=" pe-2">
+                    <img src="{{asset('assets/footer/youtube.png')}}" alt="yt" srcset="" width="30px" height="30px">
+                </a>
+            </div>
+            <p class="ms-5 text-light fw-semibold" style="font-size: 10px;">Durian Slumbung 2023 | Icons by Icon8</p>
+        </div>
+
+
+        {{-- Bagian Tengah Footer --}}
+
+
+        <div class="col-4 my-auto">
+            <nav class="nav-underline">
+                <div class="row">
+                    <div class="col">
+                        <div class="d-flex justify-content-center my-1">
+                            <a class="nav-link text-light <?= $activeHome ?>" aria-current="page" href="home.php">Home</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <div class="d-flex justify-content-center my-1">
+                            <a class="nav-link text-light <?= $activeProduct ?>" href="dapur-durian.php">Dapur Durian</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <div class="d-flex justify-content-center my-1">
+                            <a class="nav-link text-light <?= $activeWisata ?>" href="about-us.php"> About Us</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <div class="d-flex justify-content-center my-1">
+                            <a class="nav-link text-light <?= $activeAbout ?>" href="about-us.php"> About Us</a>
+                        </div>
+                    </div>
+                </div>
+
+            </nav>
+        </div>
+
+
+        {{-- Bagian Kanan Footer --}}
+
+
+        <div class="col-4">
+            <div class="row">
+                <div class="col">
+                    <div class="row align-items-bottom justify-content-center">
+                        <div class="col-1 d-flex justify-content-end">
+                            <img src="{{asset('assets/footer/location.png')}}" alt="loc" srcset="" width="30px" height="30px">
+                        </div>
+                        <div class="col-3">
+                            <h3 class="text-light">LOCATION</h3>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <p class="text-light info-footer text-center">Durkedir (Durian Kediri) 100% BERGARANSI, Meloyo, Mlancu, Kec. Kandangan, Kabupaten Kediri, Jawa Timur 64294</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <div class="row align-items-bottom justify-content-center">
+                        <div class="col-3 d-flex justify-content-end">
+                            <img src="{{asset('assets/footer/contact.png')}}" alt="loc" srcset="" width="30px" height="30px">
+                        </div>
+                        <div class="col-6">
+                            <h3 class="text-light">Contact Person</h3>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <p class="text-light info-footer text-center">WA : 0812-5975-6928 (Mulyono)<br>WA : 0812-5441-1245 (Bang Jono)</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 @endsection
