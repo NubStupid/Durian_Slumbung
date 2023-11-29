d8292e0ac941601b0b963b62583973e314f2aeaa
keliphar <gbcliffharsono@gmail.com>
2023-11-29 09:05:41 +0700

    joken babi

--- durianslumbung.sql
+++ durianslumbung.sql
@@ -11,12 +11,12 @@ create table user(
 );
 
 create table categories(
-    category_id int primary key auto_increment,
+    category_id varchar(5) primary key,
     name varchar(50)
 );
 
 create table product(
-    product_id int primary key auto_increment, 
+    product_id varchar(5) primary key,
     name varchar(100), 
     price decimal(10,2),
     category_id varchar(50) references category(category_id),
@@ -27,7 +27,7 @@ create table product(
 );
 
 create table comment(
-    comment_id int primary key auto_increment,
+    comment_id varchar(5) primary key,
     message longtext,
     username varchar(50) references user(username),
     product_id int references product(product_id),
@@ -35,26 +35,25 @@ create table comment(
 );
 
 create table rating(
-    rating_id int primary key auto_increment,
     rate int(1),
     username varchar(50) references user(username),
     product_id int references product(product_id)
 );
 
 create table likes(
-    likes_id int primary key auto_increment,
+    likes_id varchar(5) primary key,
     username varchar(50) references user(username),
     comment_id int references comment(comment_id)
 );
 
 create table h_trans(
-    h_trans_id int primary key auto_increment,
+    h_trans_id varchar(5) primary key,
     subtotal decimal(10,2) not null,
     username varchar(50) references user(username)
 );
 
 create table d_trans(
-    d_trans_id int primary key auto_increment,
+    d_trans_id varchar(5) primary key,
     qty int(5),
     total decimal(10,2) not null,
     h_trans_id int references h_trans(h_trans_id),
@@ -68,10 +67,10 @@ create table admin(
 );
 
 create table wisata(
-    wisata_id int primary key auto_increment,
+    wisata_id varchar(5) primary key,
     tgl_dipesan date,
     sesi int(1),
-    qty_orang int(2)
+    qty_orang int(5)
 );
 
 -- create table ads(
@@ -79,100 +78,156 @@ create table wisata(
 -- );
 
 create table cart(
-    cart_id int primary key auto_increment,
+    cart_id varchar(5) primary key,
     product_id int references product(product_id),
     price decimal(10,2),
     qty int(5),
     username varchar(50) references user(username)
 );
 
-insert into user (username, password, telp, poin) VALUES
-('ferdi','ferdi123','08123456789',0),
-('tasya','tasya123','08198765432',0);
+-- Insert dummy data for the user table
+INSERT INTO user (username, password, telp, poin)
+VALUES
+    ('user01', 'pass01', '12345678901', 100),
+    ('user02', 'pass02', '98765432109', 50),
+    ('user03', 'pass03', '55511122334', 75),
+    ('user04', 'pass04', '11122233344', 20),
+    ('user05', 'pass05', '99988877766', 30),
+    ('user06', 'pass06', '44455566677', 80),
+    ('user07', 'pass07', '77766655544', 60),
+    ('user08', 'pass08', '22233344455', 45),
+    ('user09', 'pass09', '66677788899', 10),
+    ('user10', 'pass10', '88899900011', 5);
 
-INSERT INTO `categories` (`category_id`, `name`) VALUES
-(1, 'Electronics'),
-(2, 'Clothing'),
-(3, 'Books'),
-(4, 'Appliances'),
-(5, 'Footwear'),
-(6, 'Fiction Books'),
-(7, 'Sports Equipment'),
-(8, 'Home and Garden'),
-(9, 'Toys'),
-(10, 'Beauty and Personal Care'),
-(11, 'Automotive'),
-(12, 'Jewelry'),
-(13, 'Movies'),
-(14, 'Music'),
-(15, 'Pet Supplies'),
-(16, 'Office Supplies'),
-(17, 'Health and Wellness'),
-(18, 'Food and Beverages'),
-(19, 'Outdoor Recreation'),
-(20, 'Art and Craft');
+-- Insert dummy data for the categories table
+INSERT INTO categories (category_id, name)
+VALUES
+    ('C0001', 'Electronics'),
+    ('C0002', 'Clothing'),
+    ('C0003', 'Books'),
+    ('C0004', 'Home and Garden'),
+    ('C0005', 'Toys'),
+    ('C0006', 'Sports and Outdoors'),
+    ('C0007', 'Beauty and Personal Care'),
+    ('C0008', 'Automotive'),
+    ('C0009', 'Health and Household'),
+    ('C0010', 'Pet Supplies');
 
-INSERT INTO `product` (`product_id`, `name`, `price`, `category_id`, `qty`, `description`, `img_url`) VALUES
-(1, 'Jonathan Kenrick\'s Laptop', '1000.00', 18, 15, 'KWNDQP:OWNKDIWQNDOPWQNDwpqd', 'https://picsum.photos/id/125/200/300'),
-(2, 'Jane\'s Smartphone', '500.00', 12, 10, 'A sleek smartphone with advanced features.', 'https://picsum.photos/id/126/200/300'),
-(3, 'Gaming Console', '300.00', 8, 20, 'Next-gen gaming console for immersive gaming experience.', 'https://picsum.photos/id/127/200/300'),
-(4, 'Wireless Headphones', '80.00', 15, 50, 'Enjoy music without the hassle of wires.', 'https://picsum.photos/id/128/200/300'),
-(5, 'Coffee Maker', '50.00', 10, 25, 'Brew your favorite coffee with ease.', 'https://picsum.photos/id/129/200/300'),
-(6, 'Fitness Tracker', '120.00', 5, 15, 'Monitor your health and stay active.', 'https://picsum.photos/id/130/200/300'),
-(7, 'Digital Camera', '250.00', 7, 12, 'Capture moments with high-quality photos.', 'https://picsum.photos/id/131/200/300'),
-(8, 'Portable Speaker', '70.00', 15, 40, 'Listen to music on the go with this portable speaker.', 'https://picsum.photos/id/132/200/300'),
-(9, 'Laptop Stand', '30.00', 18, 50, 'Improve ergonomics with this adjustable laptop stand.', 'https://picsum.photos/id/133/200/300'),
-(10, 'Bluetooth Mouse', '20.00', 18, 30, 'Wireless mouse for convenient computing.', 'https://picsum.photos/id/134/200/300'),
-(11, 'External Hard Drive', '120.00', 14, 18, 'Expand your storage capacity with this external hard drive.', 'https://picsum.photos/id/135/200/300'),
-(12, 'Desktop Monitor', '200.00', 13, 8, 'Large monitor for immersive computing experience.', 'https://picsum.photos/id/136/200/300');
+-- Insert dummy data for the product table
+INSERT INTO product (product_id, name, price, category_id, qty, description, img_url, rate)
+VALUES
+    ('P0001', 'Smartphone', 499.99, 'C001', 100, 'High-end smartphone with great features.', 'phone.jpg', 4.5),
+    ('P0002', 'T-shirt', 19.99, 'C002', 200, 'Comfortable cotton T-shirt in various colors.', 'tshirt.jpg', 4.0),
+    ('P0003', 'Programming Book', 29.99, 'C003', 50, 'Learn programming from scratch.', 'book.jpg', 4.8),
+    ('P0004', 'Coffee Maker', 89.99, 'C004', 30, 'Brew your favorite coffee at home.', 'coffee.jpg', 4.2),
+    ('P0005', 'Board Game', 24.99, 'C005', 80, 'Fun for the whole family.', 'boardgame.jpg', 4.6),
+    ('P0006', 'Outdoor Tent', 149.99, 'C006', 15, 'Perfect for camping trips.', 'tent.jpg', 4.3),
+    ('P0007', 'Skin Care Set', 39.99, 'C007', 100, 'Keep your skin healthy and radiant.', 'skincare.jpg', 4.7),
+    ('P0008', 'Car Jump Starter', 79.99, 'C008', 50, 'Never worry about a dead battery.', 'jumpstarter.jpg', 4.4),
+    ('P0009', 'Vitamins', 19.99, 'C009', 120, 'Essential vitamins for a healthy life.', 'vitamins.jpg', 4.9),
+    ('P0010', 'Pet Toy Set', 14.99, 'C010', 150, 'Entertain your furry friends.', 'pettoys.jpg', 4.1);
 
-insert into comment(comment_id, message, username, product_id) VALUES
-('0','Test123456', 'ferdi', 1),
-('0','Test123456', 'tasya', 2),
-('0','Test123456', 'ferdi', 3),
-('0','Test123456', 'ferdi', 4),
-('0','Test123456', 'tasya', 5),
-('0','Test123456', 'ferdi', 6),
-('0','Test123456', 'ferdi', 7),
-('0','Test123456', 'tasya', 8),
-('0','Test123456', 'ferdi', 9),
-('0','Test123456', 'tasya', 12);
+-- Insert dummy data for the comment table
+INSERT INTO comment (comment_id, message, username, product_id)
+VALUES
+    ('CM001', 'Great product!', 'user01', 'P0001'),
+    ('CM002', 'Love the design.', 'user02', 'P0002'),
+    ('CM003', 'Very informative book.', 'user03', 'P0003'),
+    ('CM004', 'Excellent coffee maker!', 'user04', 'P0004'),
+    ('CM005', 'Fun game for the family.', 'user05', 'P0005'),
+    ('CM006', 'Easy to set up and use.', 'user06', 'P0006'),
+    ('CM007', 'Fantastic skincare set.', 'user07', 'P0007'),
+    ('CM008', 'Saved me a few times!', 'user08', 'P0008'),
+    ('CM009', 'Best vitamins ever!', 'user09', 'P0009'),
+    ('CM010', 'My pets love these toys.', 'user10', 'P0010');
 
-insert into rating VALUES
-('0',5,'tasya',1),
-('0',4,'tasya',1),
-('0',5,'ferdi',10),
-('0',3,'tasya',9),
-('0',1,'ferdi',2),
-('0',3,'ferdi',1),
-('0',4,'ferdi',1);
+-- Insert dummy data for the rating table
+INSERT INTO rating (rate, username, product_id)
+VALUES
+    (4, 'user01', 'P0001'),
+    (5, 'user02', 'P0002'),
+    (4, 'user03', 'P0003'),
+    (5, 'user04', 'P0004'),
+    (4, 'user05', 'P0005'),
+    (5, 'user06', 'P0006'),
+    (4, 'user07', 'P0007'),
+    (5, 'user08', 'P0008'),
+    (4, 'user09', 'P0009'),
+    (5, 'user10', 'P0010');
 
-insert into likes VALUES
-('0', 'tasya', 1),
-('0', 'tasya', 3),
-('0', 'tasya', 4),
-('0', 'ferdi', 5),
-('0', 'ferdi', 2);
+-- Insert dummy data for the likes table
+INSERT INTO likes (likes_id, username, comment_id)
+VALUES
+    ('L0001', 'user01', 'CM001'),
+    ('L0002', 'user02', 'CM002'),
+    ('L0003', 'user03', 'CM003'),
+    ('L0004', 'user04', 'CM004'),
+    ('L0005', 'user05', 'CM005'),
+    ('L0006', 'user06', 'CM006'),
+    ('L0007', 'user07', 'CM007'),
+    ('L0008', 'user08', 'CM008'),
+    ('L0009', 'user09', 'CM009'),
+    ('L0010', 'user10', 'CM010');
 
-insert into h_trans VALUES
-('0', 230000, 'tasya'),
-('0', 300000, 'ferdi');
+-- Insert dummy data for the h_trans table
+INSERT INTO h_trans (h_trans_id, subtotal, username)
+VALUES
+    ('HT001', 719.98, 'user01'),
+    ('HT002', 39.98, 'user02'),
+    ('HT003', 29.99, 'user03'),
+    ('HT004', 149.99, 'user04'),
+    ('HT005', 124.95, 'user05'),
+    ('HT006', 314.97, 'user06'),
+    ('HT007', 239.92, 'user07'),
+    ('HT008', 159.98, 'user08'),
+    ('HT009', 199.80, 'user09'),
+    ('HT010', 74.95, 'user10');
 
-insert into d_trans VALUES
-('0', 2, 30000, 1, 5),
-('0', 2, 200000, 1, 3),
-('0', 4, 100000, 2, 2),
-('0', 3, 200000, 2, 6);
+-- Insert dummy data for the d_trans table
+INSERT INTO d_trans (d_trans_id, qty, total, h_trans_id, product_id)
+VALUES
+    ('DT001', 2, 499.99, 'HT001', 'P0001'),
+    ('DT002', 1, 19.99, 'HT002', 'P0002'),
+    ('DT003', 1, 29.99, 'HT003', 'P0003'),
+    ('DT004', 1, 89.99, 'HT004', 'P0004'),
+    ('DT005', 3, 74.97, 'HT005', 'P0005'),
+    ('DT006', 1, 149.99, 'HT006', 'P0006'),
+    ('DT007', 2, 79.98, 'HT007', 'P0007'),
+    ('DT008', 1, 39.99, 'HT008', 'P0008'),
+    ('DT009', 2, 39.98, 'HT009', 'P0009'),
+    ('DT010', 1, 14.99, 'HT010', 'P0010');
 
-insert into admin VALUES
-('blaba', 'hiu', 'a'),
-('blebe', 'bebek', 'a'),
-('mulyono', '123', 'm');
+-- Insert dummy data for the admin table
+INSERT INTO admin (username, password, role)
+VALUES
+    ('admin01', 'adminpass01', 'A'),
+    ('admin02', 'adminpass02', 'A'),
+    ('mulyono', '123', 'M');
 
-insert into cart VALUES
-('0', 1, 300000, 5, 'tasya'),
-('0', 2, 100000, 3, 'ferdi');
+-- Insert dummy data for the wisata table
+INSERT INTO wisata (wisata_id, tgl_dipesan, sesi, qty_orang)
+VALUES
+    ('W0001', '2023-12-01', 1, 5),
+    ('W0002', '2023-12-02', 2, 8),
+    ('W0003', '2023-12-03', 1, 3),
+    ('W0004', '2023-12-04', 2, 6),
+    ('W0005', '2023-12-05', 1, 4),
+    ('W0006', '2023-12-06', 2, 10),
+    ('W0007', '2023-12-07', 1, 7),
+    ('W0008', '2023-12-08', 2, 12),
+    ('W0009', '2023-12-09', 1, 9),
+    ('W0010', '2023-12-10', 2, 15);
 
-insert into wisata (wisata_id, sesi, qty_orang) VALUES
-('0', 2, 50),
-('0', 1, 20);
+-- Insert dummy data for the cart table
+INSERT INTO cart (cart_id, product_id, price, qty, username)
+VALUES
+    ('C0001', 'P0001', 499.99, 2, 'user01'),
+    ('C0002', 'P0002', 19.99, 1, 'user02'),
+    ('C0003', 'P0003', 29.99, 1, 'user03'),
+    ('C0004', 'P0004', 89.99, 1, 'user04'),
+    ('C0005', 'P0005', 24.99, 3, 'user05'),
+    ('C0006', 'P0006', 149.99, 1, 'user06'),
+    ('C0007', 'P0007', 39.99, 2, 'user07'),
+    ('C0008', 'P0008', 79.99, 1, 'user08'),
+    ('C0009', 'P0009', 19.99, 2, 'user09'),
+    ('C0010', 'P0010', 14.99, 1, 'user10');
